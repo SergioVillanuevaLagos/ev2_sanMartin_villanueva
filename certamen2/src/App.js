d013f03aa6697e28555471a34bb59d74b2ba1977
SergioVillanuevaLagos <sergio.villanueva2101@alumnos.ubiobio.cl>
2025-11-18 16:24:53 +0000

    Subida de datos base

---
+++ certamen2/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import "./css/bootstrap.min.css";
+
+function App() {
+  return ( 
+    <Fragment>
+      <h1>Hola Mundo</h1>
+    </Fragment>
+  ); 
+}
+
+export default App;
